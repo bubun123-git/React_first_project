979b2908cea670ee87d248fa36dd43755ec265e4
SWAPNA SARIT MALLICK <bubundcool@gmail.com>
2023-06-22 11:37:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Expenseitem from "./components/ExpensesItem";
+
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenseitem></Expenseitem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpensesItem.js
@@ -0,0 +1,11 @@
+function Expenseitem() {
+  return (
+    <div>
+      <div>Food Rs 10</div>
+      <div>Petrol Rs 100</div>
+      <div>Movies Rs 200</div>
+    </div>
+  );
+}
+
+export default Expenseitem;
