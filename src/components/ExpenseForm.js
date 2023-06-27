08b9e05b7b9e18f2c6ca8b8abc844550971a306c
bubun123-git <130881036+bubun123-git@users.noreply.github.com>
2023-06-27 13:04:01 +0530

    introduce state inside the new Component

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpense.css";
 
 function ExpenseForm() {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  const [enteredPlace, setEnteredPlace] = useState("");
+
   const titleChangeHandler = (event) => {
-    console.log(event.target.value);
+    setEnteredTitle(event.target.value);
   };
-  const AmountChangeHandler = (event) => {
-    console.log(event.target.value);
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
   };
-  const DateChangeHandler = (event) => {
-    console.log(event.target.value);
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
   };
-  const PlaceChangeHandler = (event) => {
-    console.log(event.target.value);
+
+  const placeChangeHandler = (event) => {
+    setEnteredPlace(event.target.value);
   };
+
   return (
     <form>
       <div className="new-expense__controls">
@@ -33,9 +42,10 @@ function ExpenseForm() {
             placeholder="Enter amount"
             min="0.01"
             step="0.01"
-            onChange={AmountChangeHandler}
+            onChange={amountChangeHandler}
           />
         </div>
+        
         <div className="new-expense__control">
           <label>Date</label>
           <input
@@ -43,14 +53,15 @@ function ExpenseForm() {
             placeholder="Enter date"
             min="2019-01-01"
             max="2022-12-31"
-            onChange={DateChangeHandler}
+            onChange={dateChangeHandler}
           />
         </div>
+        
         <div className="new-expense__control">
           <label>Place</label>
           <input
             type="text"
-            onChange={PlaceChangeHandler}
+            onChange={placeChangeHandler}
             placeholder="Enter place"
           />
         </div>
