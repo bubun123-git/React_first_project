7a216990311de7dce64ae77c442a4e0a8bdd3630
bubun123-git <130881036+bubun123-git@users.noreply.github.com>
2023-06-27 12:06:30 +0530

    added eventListner

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -2,25 +2,57 @@ import React from "react";
 import "./NewExpense.css";
 
 function ExpenseForm() {
+  const titleChangeHandler = (event) => {
+    console.log(event.target.value);
+  };
+  const AmountChangeHandler = (event) => {
+    console.log(event.target.value);
+  };
+  const DateChangeHandler = (event) => {
+    console.log(event.target.value);
+  };
+  const PlaceChangeHandler = (event) => {
+    console.log(event.target.value);
+  };
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" placeholder="Enter title" />
+          <input
+            type="text"
+            onChange={titleChangeHandler}
+            placeholder="Enter title"
+          />
         </div>
 
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" placeholder="Enter amount" min="0.01" step="0.01" />
+          <input
+            type="number"
+            placeholder="Enter amount"
+            min="0.01"
+            step="0.01"
+            onChange={AmountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" placeholder="Enter date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            placeholder="Enter date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={DateChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Place</label>
-          <input type="text" placeholder="Enter place" />
+          <input
+            type="text"
+            onChange={PlaceChangeHandler}
+            placeholder="Enter place"
+          />
         </div>
       </div>
       <div className="new-expense__actions">
