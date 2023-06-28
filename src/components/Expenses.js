384ea01d93048b437a819bb61a99bb01ff26cb1f
bubun123-git <130881036+bubun123-git@users.noreply.github.com>
2023-06-28 14:05:40 +0530

    Update Expenses.js

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,36 +1,39 @@
+import React from "react";
 import Card from "./Card";
-import Expenseitem from "./ExpensesItem";
-import './Expenses.css'
-
+import ExpenseItem from "./ExpensesItem"; // Corrected component name
+import "./Expenses.css";
 
 function Expenses(props) {
   return (
-    <Card className="expenses">
-      <Expenseitem>
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-        place={props.items[0].place}
-      </Expenseitem>
-      <Expenseitem>
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-        place={props.items[1].place}
-      </Expenseitem>
-      <Expenseitem>
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-        place={props.items[2].place}
-      </Expenseitem>
-      <Expenseitem>
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-        place={props.items[3].place}
-      </Expenseitem>
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpenseItem
+          title={props.items[0].title}
+          amount={props.items[0].amount}
+          date={props.items[0].date}
+          place={props.items[0].place}
+        />
+        <ExpenseItem
+          title={props.items[1].title}
+          amount={props.items[1].amount}
+          date={props.items[1].date}
+          place={props.items[1].place}
+        />
+        <ExpenseItem
+          title={props.items[2].title}
+          amount={props.items[2].amount}
+          date={props.items[2].date}
+          place={props.items[2].place}
+        />
+        <ExpenseItem
+          title={props.items[3].title}
+          amount={props.items[3].amount}
+          date={props.items[3].date}
+          place={props.items[3].place}
+        />
+      </Card>
+    </div>
   );
 }
+
 export default Expenses;
