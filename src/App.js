e9fab49036b10cdcdf0454879ff3b71f8295c3c3
bubun123-git <130881036+bubun123-git@users.noreply.github.com>
2023-07-01 12:46:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,46 @@
-import React from "react";
-import Expenseitem from "./components/ExpensesItem";
+import React, { useState } from "react";
+
 import NewExpense from "./components/NewExpense";
 import Expenses from "./components/Expenses";
 
+const DummyExpenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+    place: "ctc",
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+    place: "rourkela",
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+    place: "balangir",
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+    place: "bbsr",
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      place: "ctc",
-    },
-    {
-      id: "e2",
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-      place: "rourkela",
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-      place: "balangir",
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      place: "bbsr",
-    },
-  ];
-  
+  const [expenses, setExpenses] = useState(DummyExpenses);
+
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
